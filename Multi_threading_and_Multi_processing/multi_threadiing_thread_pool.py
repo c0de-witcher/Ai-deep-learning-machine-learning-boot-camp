b576bed79cb2aff834a0f8cb2ac58274510272d4
c0de-witcher <adityamsrivastava9@gmail.com>
2025-07-29 11:53:10 +0530

    multi threading with thread pool

---
+++ Multi_threading_and_Multi_processing/multi_threadiing_thread_pool.py
@@ -0,0 +1,15 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def print_number( number):
+   time.sleep(2)
+   return f"Number {number}"
+
+numbers = [0, 1, 2, 3, 4]
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+   results = executor.map(print_number,numbers)
+
+for result in results:
+   print(result)
+    
