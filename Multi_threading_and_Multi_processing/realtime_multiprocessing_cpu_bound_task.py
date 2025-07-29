f821999f7ca4a0a85c9deab8bbed5dd34a93eca4
c0de-witcher <adityamsrivastava9@gmail.com>
2025-07-29 13:02:21 +0530

    real world multi processing

---
+++ Multi_threading_and_Multi_processing/realtime_multiprocessing_cpu_bound_task.py
@@ -0,0 +1,24 @@
+import multiprocessing
+import math
+import time
+import sys
+
+#sys.set_int_max_str_digits(100000)
+
+def computer_factorial(number):
+    print(f"Computing factorial of {number}")
+    result = math.factorial(number)
+    print(f"Factorial of {number} is {result}")
+    return result
+
+numbers = [500,600]
+
+start_time = time.time()
+
+with multiprocessing.Pool() as pool:
+    results = pool.map(computer_factorial,numbers)
+
+end_time = time.time()
+
+print(f"Results: {results}")
+print(f"Time taken: {end_time - start_time} seconds")
